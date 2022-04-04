24c0a928bfc1ee21365e1c08cbd1a03f1c1f674a
Tom <tom@toms-macbook-pro-2.home>
2022-04-05 00:10:42 +0200

    new functions: interest over time

--- dags/google_trends.py
+++ dags/google_trends.py
@@ -5,17 +5,16 @@ import time
 from datetime import datetime, timedelta
 from sqlalchemy import create_engine
 import top_technologies
-#from airflow.models import Variable
+from airflow.models import Variable
 
 ## Load credentials
-USER = "tom" #Variable.get("GT_USER")
-PASSWORD = "google_trends_123" #Variable.get("GT_PASSWORD")
-ENDPOINT = "datalakegt.cettyoj69g9b.us-east-1.rds.amazonaws.com" #Variable.get("GT_ENDPOINT")
-PORT = 5432 #Variable.get("GT_PORT")
-DATABASENAME = "datalakeGT" #Variable.get("GT_DATABASENAME")
+GT_ENDPOINT = Variable.get('GT_ENDPOINT')
+GT_DB_NAME = Variable.get('GT_DB_NAME')
+GT_USERNAME = Variable.get('GT_USERNAME')
+GT_PASSWORD = Variable.get('GT_PASSWORD')
 
 ## Define database connection
-url = f"postgresql://{USER}:{PASSWORD}@{ENDPOINT}:{PORT}/{DATABASENAME}"
+url = f"postgresql://{GT_USERNAME}:{GT_PASSWORD}@{GT_ENDPOINT}/{GT_DB_NAME}"
 engine = create_engine(url)
 
 def google_trends():
@@ -115,9 +114,93 @@ def google_trends():
       df_final = df_final.groupby(by=["date","technology"]).mean().round()
 
       ## Load data into database
-      load_data(df_final, gt_type)
+      load_data(df_final, gt_type, "append")
 
-    def load_data(df, table):
+
+    def gt_interest_over_time_combo(kw_list):
+      """
+      Retrieving and Loading the data from the Google Trends API regaring the historical interest of defined key words from the Dev-Jobs function.
+      """
+
+      ## Google Trends API type/kpi
+      gt_type = "gt_interest_over_time_combo"
+
+      ## Define Google Trends object
+      pytrends = TrendReq()
+
+      ## DataFrame template
+      df_final = pd.DataFrame({"date": [], "technology": [], "value": []})
+
+      ## Unique combinations of keywords
+      combos = list(combinations(kw_list,2))
+
+      for combo in combos:
+        ## Get results from Google Trends for each keyword combo
+        pytrends.build_payload(kw_list = list(combo)
+                               ,geo = "CH"
+                               ,timeframe="all")
+
+        ## Interest over time
+        df = pytrends.interest_over_time()
+
+        ## Time buffer between requests
+        time.sleep(0.3)
+
+        ## Adding retrieved data to existing DataFrame
+        try:
+          df = df[list(combo)]
+          df.reset_index(inplace=True)
+          df = df.melt(id_vars=["date"], value_vars=combo, var_name="technology", value_name="value")
+          df_final = pd.concat([df_final, df])
+        except:
+          continue
+
+      ## Mean-Score per technology
+      df_final = df_final.groupby(by=["date","technology"]).mean().round()
+
+      ## Load data into database
+      load_data(df_final, gt_type, "replace")
+
+    def gt_interest_over_time_single(kw_list):
+      """
+      Retrieving and Loading the data from the Google Trends API regaring the historical interest of defined key words from the Dev-Jobs function.
+      """
+
+      ## Google Trends API type/kpi
+      gt_type = "gt_interest_over_time_single"
+
+      ## Define Google Trends object
+      pytrends = TrendReq()
+
+      ## DataFrame template
+      df_final = pd.DataFrame({"date": [], "technology": [], "value": []})
+
+      for keyword in kw_list:
+        ## Get results from Google Trends for each individual keyword
+        pytrends.build_payload(kw_list = [keyword]
+                               ,geo = "CH"
+                               ,timeframe="all")
+
+        ## Interest over time
+        df = pytrends.interest_over_time()
+
+        ## Time buffer between requests
+        time.sleep(0.3)
+
+        ## Adding retrieved data to existing DataFrame
+        try:
+          df = df[[keyword]]
+          df.reset_index(inplace=True)
+          df = df.melt(id_vars=["date"], value_vars=[keyword], var_name="technology", value_name="value")
+          df_final = pd.concat([df_final, df])
+        except:
+          continue
+
+      ## Load data into database
+      load_data(df_final, gt_type, "replace")
+
+
+    def load_data(df, table, load):
       """
       Loading the data from a DataFrame to a database table.
       """
@@ -125,11 +208,13 @@ def google_trends():
       with engine.begin() as connection:
         df.to_sql(table,
                   con=connection,
-                  if_exists="append",
+                  if_exists=load,
                   method="multi")
-      print("Loading process complete.")
+      print(f"Loading process '{table}' complete.")
 
     # -----------------------
 
     kw_list = retrieve_top_technologies()
     gt_historical_interest(kw_list, 3)
+    gt_interest_over_time_combo(kw_list)
+    gt_interest_over_time_single(kw_list)
