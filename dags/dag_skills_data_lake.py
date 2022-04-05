78ea55dc7ace51e2739eb27b33ca0309d2adbfff
Tom <tom@toms-macbook-pro-2.home>
2022-04-05 08:26:29 +0200

    New Nodes: top_technologies, google_trends

--- dags/dag_skills_data_lake.py
+++ dags/dag_skills_data_lake.py
@@ -6,6 +6,8 @@ from airflow.operators.python_operator import PythonOperator
 import functions_stack_exchange
 import job_request
 import stack_exchange_handler
+import top_technologies
+import google_trends
 
 dag = DAG('dag_skills_data_lake', start_date=datetime.datetime(2022, 4, 1), schedule_interval='@daily')
 
@@ -28,5 +30,19 @@ stack_exchange_collection = PythonOperator(
    dag=dag
 )
 
+import_top_technologies = PythonOperator(
+   task_id="import_top_technologies",
+   python_callable=top_technologies.import_top_technologies,
+   dag=dag
+)
+
+google_trends_collection = PythonOperator(
+   task_id="google_trends_collection",
+   python_callable=google_trends.google_trends,
+   dag=dag
+)
+
 greet_task >> dev_job_collection
 dev_job_collection >> stack_exchange_collection
+dev_job_collection >> import_top_technologies
+import_top_technologies >> google_trends_collection
