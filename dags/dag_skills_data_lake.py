9b53f094ce02ad8933cd868e5bfdd8486b9357d8
Leo Rettich <leo.rettich@stud.hslu.ch>
2022-04-08 10:55:23 +0200

    Technical restructuring of the DAG in order to reduce the top level python code, which should prevent the occurance of the BagDag import timeout error

--- dags/dag_skills_data_lake.py
+++ dags/dag_skills_data_lake.py
@@ -3,43 +3,58 @@ import datetime
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 
-import functions_stack_exchange
-import job_request
-import stack_exchange_handler
-import top_technologies
-import google_trends
-
 dag = DAG('dag_skills_data_lake', start_date=datetime.datetime(2022, 4, 1), schedule_interval='@daily')
+
+
+def call_job_request():
+    import job_request
+    job_request.job_request()
+
+def call_collect_stack_exchange_data(**kwargs):
+    import stack_exchange_handler
+    stack_exchange_handler.collect_stack_exchange_data(kwargs)
+
+def call_clean_stack_exchange_data():
+    import stack_exchange_handler
+    stack_exchange_handler.clean_stack_exchange_data()
+
+def call_import_top_technologies():
+    import top_technologies
+    top_technologies.import_top_technologies()
+
+def call_google_trends():
+    import google_trends
+    google_trends.google_trends
 
 
 dev_job_collection = PythonOperator(
    task_id="dev_job_collection",
-   python_callable=job_request.job_request,
+   python_callable=call_job_request,
    dag=dag
 )
 
 stack_exchange_collection = PythonOperator(
    task_id="stack_exchange_collection",
-   python_callable=stack_exchange_handler.collect_stack_exchange_data,
+   python_callable=call_collect_stack_exchange_data,
    provide_context=True,
    dag=dag
 )
 
 stack_exchange_cleaning = PythonOperator(
    task_id="stack_exchange_cleaning",
-   python_callable=stack_exchange_handler.clean_stack_exchange_data,
+   python_callable=call_clean_stack_exchange_data,
    dag=dag
 )
 
 import_top_technologies = PythonOperator(
    task_id="import_top_technologies",
-   python_callable=top_technologies.import_top_technologies,
+   python_callable=call_import_top_technologies,
    dag=dag
 )
 
 google_trends_collection = PythonOperator(
    task_id="google_trends_collection",
-   python_callable=google_trends.google_trends,
+   python_callable=call_google_trends,
    dag=dag
 )
 
